e00bce6c7617c8004b3013d2a29349a5b9e1ce0c
user <user@DESKTOP-4NMVE1B>
2021-11-08 12:47:39 +0900

    asd

--- 20211108/src/Ex_07.java
+++ 20211108/src/Ex_07.java
@@ -1,10 +1,7 @@
-package Ä¿¹Ô;
 
-public class Ä¿¾î¾û¹Ô {
+public class Ex_07 {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Ä¿¾î¾î¾û¹Ô");
 		// TODO Auto-generated method stub
 
 	}
